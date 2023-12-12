b181d4f118da8a5a5cd8bc55f06aa1311c8061aa
David <david1821dev@gmail.com>
2023-12-13 00:17:25 +0300

    OpenZeppelin v2 erc20 detector fix

--- src/core/constants/openzeppelinv2_erc20.py
+++ src/core/constants/openzeppelinv2_erc20.py
@@ -10,10 +10,18 @@ from src.core.constants.erc20 import (
 ZEPPELIN_V2_ERC20_EVENTS = ERC20_EVENTS
 ZEPPELIN_V2_ERC20_FUNCTIONS = ERC20_FUNCTIONS + [
     FunctionSignature(
-        "increaseAllowance", ["address", "uint256"], None, False, [ERC20_APPROVAL_EVENT]
+        "increaseAllowance",
+        ["address", "uint256"],
+        "bool",
+        False,
+        [ERC20_APPROVAL_EVENT],
     ),
     FunctionSignature(
-        "decreaseAllowance", ["address", "uint256"], None, False, [ERC20_APPROVAL_EVENT]
+        "decreaseAllowance",
+        ["address", "uint256"],
+        "bool",
+        False,
+        [ERC20_APPROVAL_EVENT],
     ),
     FunctionSignature(
         "_transfer",
